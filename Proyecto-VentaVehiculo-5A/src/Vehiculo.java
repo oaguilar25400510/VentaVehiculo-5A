0bc44d296bcebcd18d92c763213a6b382a9442fb
oaguilar25400510 <oraguilares@ittepic.edu.mx>
2026-02-13 10:18:34 -0700

    Clase Vehiculo completa

---
+++ Proyecto-VentaVehiculo-5A/src/Vehiculo.java
@@ -0,0 +1,18 @@
+public class Vehiculo {
+    private String marca;
+    private double precio;
+    private String motor;
+    
+    public Vehiculo(String marca, double precio, String motor){
+        this.marca=marca;
+        this.precio=precio;
+        this.motor=motor;
+    }
+    
+    @Override
+    public String toString(){
+        return "Marca = "+this.marca +"\n"+
+                "Precio = "+this.precio +"\n"+
+                "Motor = "+this.motor+"\n";
+    }
+}
